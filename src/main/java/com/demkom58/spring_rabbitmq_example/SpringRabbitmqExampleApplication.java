471d48972de57093d8a24cf3d18bbe5f25c87b6c
Max Demydenko <m_demy@pm.me>
2022-11-20 22:54:03 +0200

    feat: initial commit

---
+++ src/main/java/com/demkom58/spring_rabbitmq_example/SpringRabbitmqExampleApplication.java
@@ -0,0 +1,9 @@
+package com.demkom58.spring_rabbitmq_example;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringRabbitmqExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitmqExampleApplication.class, args);
+    }
+}
